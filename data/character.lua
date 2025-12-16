28a99ea895c9f7172614c4390cbd1118b65c9402
Sky Leite <sky@leite.dev>
2025-12-16 15:28:39 -0300

    Add Akiha c5b1

--- data/character.lua
+++ data/character.lua
@@ -5,6 +5,7 @@ return {
     spritesheets = {
         standing = { file = "akiha_idle.png", columns = 12, rows = 1, width = 3072, height = 256 },
         c5a = { file = "akiha_c5a.png", columns = 6, rows = 1, width = 1536, height = 256 },
+        c5b1 = { file = "akiha_c5b1.png", columns = 10, rows = 1, width = 2560, height = 256 },
         walk_forward = { file = "akiha_walk_forward.png", columns = 13, rows = 1, width = 3328, height = 256 },
         walk_backward = { file = "akiha_walk_backward.png", columns = 13, rows = 1, width = 3328, height = 256 },
     },
@@ -22,15 +23,22 @@ return {
             },
             cancels = {
                 c5a = {},
+                c5b1 = {},
             }
         },
         walk_forward = {
             commands = {},
-            cancels = {}
+            cancels = {
+                c5a = {},
+                c5b1 = {},
+            }
         },
         walk_backward = {
             commands = {},
-            cancels = {}
+            cancels = {
+                c5a = {},
+                c5b1 = {},
+            }
         },
         c5a = {
             commands = {
@@ -43,6 +51,19 @@ return {
                 },
             },
             input = "A",
+            cancels = {}
+        },
+        c5b1 = {
+            commands = {
+                {
+                    action = {
+                        SetHitboxes = {
+                            { x = 0, y = 0, w = 10, h = 10 }
+                        }
+                    },
+                },
+            },
+            input = "B",
             cancels = {}
         },
     },
@@ -73,6 +94,24 @@ return {
             }
         },
         c5a = {
+            Sprite = {
+                default_collision_box = {
+                    x = 0,
+                    y = -35,
+                    w = 30,
+                    h = 100
+                },
+                frames = {
+                    { cell = { 0, 0 }, duration = 4 },
+                    { cell = { 1, 0 }, duration = 5 },
+                    { cell = { 2, 0 }, duration = 6 },
+                    { cell = { 3, 0 }, duration = 6 },
+                    { cell = { 4, 0 }, duration = 6 },
+                    { cell = { 5, 0 }, duration = 6 },
+                }
+            }
+        },
+        c5b1 = {
             Sprite = {
                 default_collision_box = {
                     x = 0,
